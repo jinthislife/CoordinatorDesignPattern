8f942b776cb830328547c63e80f2f1838c8e8972
Jin Lee <devkjlee@gmail.com>
2019-10-10 09:11:15 +1000

    Basic Coordinator operation

---
+++ CoordinatorTest/CoordinatorTest/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  CoordinatorTest
+//
+//  Created by Jin Lee on 10/10/19.
+//  Copyright © 2019 Jin Lee. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CoordinatorTest/CoordinatorTest/ThirdViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ThirdViewController.swift
+//  CoordinatorTest
+//
+//  Created by Jin Lee on 9/10/19.
+//  Copyright © 2019 Jin Lee. All rights reserved.
+//
+
+import Foundation
